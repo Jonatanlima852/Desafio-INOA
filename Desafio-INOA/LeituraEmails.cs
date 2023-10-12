1f91b8ac285238bf90f1bb9a371d36f4b6f53094
Jonatan de Lima <jonatanlima852@gmail.com>
2023-10-12 16:57:38 -0300

    add: reading config json

---
+++ Desafio-INOA/LeituraEmails.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Mail;
+using System.Net;
+using System.Text.Json.Nodes;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace Desafio_INOA
+{
+    internal class LeituraEmails
+    {
+
+        public class ConfigSMTP
+        {
+
+            public string Host { get; set; }
+            public string Username { get; set; }
+            public string Password { get; set; }
+            public int Port { get; set; }
+            public List<string> Emails { get; set; }
+        }
+
+        public List<string> EmailsDestino { get; set; }
+
+        public LeituraEmails(string arquivo)
+        {
+            
+            if(!File.Exists(arquivo))
+            {
+                Console.WriteLine("Uai");
+                Console.WriteLine(arquivo);
+                Console.WriteLine("Arquivo de configuração não foi encontrado.");
+                return;
+            }
+
+            using (Stream entrada = File.Open(arquivo, FileMode.Open))
+            using (StreamReader configFile = new StreamReader(entrada))
+            {
+                string json = configFile.ReadToEnd();
+                ConfigSMTP config = JsonConvert.DeserializeObject<ConfigSMTP>(json);
+
+
+                // Crie uma instância da classe SmtpClient
+                SmtpClient client = new SmtpClient();
+
+                // Especifique seu host de correspondência, nome de usuário, senha, número da porta e opção de segurança
+                // client.Host = "mail.server.com";
+                // client.Credentials = new NetworkCredential("seuemail@gmail.com", "suasenha");
+                // client.Port = 587;
+
+                List<string> emails = config.Emails;
+
+                client.Host = config.Host;
+                client.Credentials = new NetworkCredential(config.Username, config.Password);
+                client.EnableSsl = true; // Use SSL para conexão segura
+                client.Port = config.Port;
+                this.EmailsDestino = emails;
+            }
+
+        }
+
+    }
+}
--- Desafio-INOA/Program.cs
+++ Desafio-INOA/Program.cs
@@ -1,2 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Desafio_INOA;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Verifique se um argumento de linha de comando (nome do arquivo) foi fornecido.
+        if (args.Length < 0)
+        {
+            Console.WriteLine("Não foram fornecidos todos os parâmetros." +
+                "Forneça a ação, o limite superior e o limite inferior");
+            return;
+        }
+
+        //string nomeArquivoConfig = "configuracao.json";
+
+        string nomeArquivoConfig = "C:\\Users\\Jonatan\\source\\repos\\Desafio-INOA\\configuracao.json";
+
+        //string nomeArquivoConfig = args[0]; // O primeiro argumento é o nome do arquivo.
+        //int limiteInf = int.Parse(args[1]); // O segundo argumento é o preço inferior
+        //int limiteSup = int.Parse(args[2]); // O terceiro argumento é o preço superior
+
+        // Crie uma instância de LeituraEmails passando o nome do arquivo de configuração.
+        LeituraEmails leitor = new LeituraEmails(nomeArquivoConfig);
+
+        // Agora você pode acessar a propriedade EmailsDestino na instância leitor.
+        foreach (string email in leitor.EmailsDestino)
+        {
+            Console.WriteLine("Email de destino: " + email);
+        }
+    }
+}
